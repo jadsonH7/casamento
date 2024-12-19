2fd51681d980efee658727c35efdf5fb868c66df
Jadson <jadsonzap@gmail.com>
2024-12-19 01:25:52 -0300

    Atualizando links

---
+++ src/frontend/javascript/login.js
@@ -0,0 +1,7 @@
+const entrar = document.querySelector(".auth-form-login");
+const cadastrar = document.querySelector(".auth-form-register");
+const btnEntrar = document.querySelector(".toggle-login, strong");
+const btnCadastrar = document.querySelector(".toggle-register, strong");
+
+function toggleLogin() {};
+toggleLogin()
