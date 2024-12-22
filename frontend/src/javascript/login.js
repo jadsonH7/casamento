8706765ebb5be15ce5d80dd1eed67d8859c6464d
Jadson <jadsonzap@gmail.com>
2024-12-22 15:01:39 -0300

    Inclusão de paginas Login/Register

--- frontend/src/javascript/login.js
+++ frontend/src/javascript/login.js
@@ -1,7 +1,26 @@
-const entrar = document.querySelector(".auth-form-login");
-const cadastrar = document.querySelector(".auth-form-register");
-const btnEntrar = document.querySelector(".toggle-login, strong");
-const btnCadastrar = document.querySelector(".toggle-register, strong");
+// Toggle from Login to register
+const login = document.querySelector(".auth-form-login");
+const register = document.querySelector(".auth-form-register");
+const btnLogin = document.querySelector(".btn-login");
+const btnRegister = document.querySelector(".btn-register");
 
-function toggleLogin() {};
-toggleLogin()
+function toggleLogin() {
+    // e.preventDefault()
+    register.style.display = "none";
+
+    btnLogin.addEventListener("click", () => {
+        login.style.display = "none";
+        if (register.style.display === "none") {
+            register.style.display = "flex";
+        }
+    });
+
+    btnRegister.addEventListener("click", () => {
+        register.style.display = "none";
+        if (login.style.display === "none") {
+            login.style.display = "flex";
+            login.style.heigth
+        }
+    });
+};
+toggleLogin();
